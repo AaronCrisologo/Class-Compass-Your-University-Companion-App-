d8936f0581f5daff05901ea979f2177c9ce994fe
AaronCrisologo <21-08166@g.batstate-u.edu.ph>
2024-11-13 18:33:04 +0800

    Removing Unnecesary UI Elements Removing features that were at the frontend app and only leaving those that will be built with a backend

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'home_screen.dart';
 import 'calendar_screen.dart';
-import 'notifications_screen.dart';
 import 'announcements_screen.dart';
 import 'schedule_screen.dart';
-import 'settings_screen.dart';
 import 'login_screen.dart';
 import 'profile_screen.dart';
-import 'archivednotifications_screen.dart';
-import 'blockedaccounts_screen.dart';
-import 'resources.dart';
 
 void main() => runApp(ClassCompassApp());
 
@@ -60,7 +55,6 @@ class _MainScreenState extends State<MainScreen>
   static final List<Widget> _widgetOptions = <Widget>[
     HomeScreen(),
     CalendarScreen(),
-    NotificationScreen(),
     AnnouncementsScreen(),
     ScheduleScreen(),
   ];
@@ -90,13 +84,6 @@ class _MainScreenState extends State<MainScreen>
     });
   }
 
-  void _logout() {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => LoginScreen()),
-    );
-  }
-
   void _navigateToProfile() {
     Navigator.push(
       context,
@@ -111,10 +98,8 @@ class _MainScreenState extends State<MainScreen>
       case 1:
         return 'Calendar';
       case 2:
-        return 'Notifications';
-      case 3:
         return 'Announcements';
-      case 4:
+      case 3:
         return 'Schedule';
       default:
         return 'Class Compass';
@@ -157,12 +142,12 @@ class _MainScreenState extends State<MainScreen>
         actions: [
           IconButton(
             icon: Icon(
-              Icons.settings,
+              Icons.logout,
             ),
             onPressed: () {
-              Navigator.push(
+              Navigator.pushReplacement(
                 context,
-                _createRoute(SettingsScreen()),
+                MaterialPageRoute(builder: (context) => LoginScreen()),
               );
             },
           ),
@@ -183,10 +168,6 @@ class _MainScreenState extends State<MainScreen>
             label: 'Calendar',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Notifications',
-          ),
-          BottomNavigationBarItem(
             icon: Icon(
               FontAwesomeIcons.bullhorn,
               size: 22,
@@ -204,78 +185,6 @@ class _MainScreenState extends State<MainScreen>
         showUnselectedLabels: false,
         type: BottomNavigationBarType.fixed,
         iconSize: Theme.of(context).iconTheme.size ?? 24,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Aaron Angelo Crisologo"),
-              accountEmail: Text("21-08166@g.batstate-u.edu.ph"),
-              currentAccountPicture: GestureDetector(
-                onTap: _navigateToProfile,
-                child: CircleAvatar(
-                  backgroundImage: NetworkImage("assets/profile.png"),
-                  backgroundColor: Colors.red[600],
-                ),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.red[800],
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('My Profile'),
-              onTap: _navigateToProfile,
-            ),
-            ListTile(
-              leading: Icon(Icons.book),
-              title: Text('Resources'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ResourceScreen()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.block),
-              title: Text('Blocked Accounts'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => BlockedAccountsScreen()),
-                );
-              },
-            ),
-            ListTile(
-                leading: Icon(Icons.archive),
-                title: Text('Notification Archive'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ArchivedNotificationsScreen()),
-                  );
-                }),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SettingsScreen()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Logout'),
-              onTap: _logout,
-            ),
-          ],
-        ),
       ),
     );
   }
